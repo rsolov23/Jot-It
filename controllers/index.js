7090eef43f41bfc56df9227107ae35418fabee50
Nathanael White <narhanaelw99@gmail.com>
2021-06-23 17:31:29 -0500

    uncommented two lines

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api/");
-// const homeRoutes = require("./home-routes.js");
+const homeRoutes = require("./home-routes.js");
 
 const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/dashboard", dashboardRoutes);
-// router.use("/", homeRoutes);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
