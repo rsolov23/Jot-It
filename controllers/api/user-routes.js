fd383b9370e993d4119152c825be968658fc907b
Rachel Solov <rsolov23@yahoo.com>
2021-06-23 13:16:58 -0500

    added get route for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,29 @@
 //require router
+const router = require("express").Router();
 //require models
+const { User, Post } = require("../../models");
 
 //get all
-
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 //get by id
-
+// router.get("/:id", (req,res) => {
+//     User.findOne({
+//         attributes: { exclude: ["password"]},
+//         where: {
+//             id: req.params.id,
+//         },
+//         include: []
+//     })
+// })
 //post route=create
 
 //post login
@@ -16,3 +35,4 @@
 //destroy user??
 
 //module exports
+module.exports = router;
