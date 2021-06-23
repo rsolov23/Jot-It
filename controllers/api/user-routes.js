72368f2b19d61ad2cd7196f61e0efcb3f6907fd7
Rachel Solov <rsolov23@yahoo.com>
2021-06-23 14:02:57 -0500

    completed user/router.post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,16 +15,58 @@ router.get("/", (req, res) => {
     });
 });
 //get by id
-// router.get("/:id", (req,res) => {
-//     User.findOne({
-//         attributes: { exclude: ["password"]},
-//         where: {
-//             id: req.params.id,
-//         },
-//         include: []
-//     })
-// })
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_text", "created_at"],
+      },
+
+      {
+        model: Post,
+        attributes: ["title"],
+        through: User,
+        as: "user_id",
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 //post route=create
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //post login
 
